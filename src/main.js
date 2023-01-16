7685cfd91eb24ac3c538df3d88d94e5ad0ccdf98
acmarchiori <ac_marchiori@yahoo.com.br>
2023-01-16 19:55:45 -0300

    Finalizado requisito 3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const products = document.querySelector('.products');
 
-const teste = await fetchProductsList('computador');
-console.log(teste);
+const getProducts = async () => {
+  const product = await fetchProductsList('computador');
+  product.forEach((e) => {
+    const data = createProductElement(e);
+    console.log(data);
+    products.appendChild(data);
+  });
+};
+getProducts();
