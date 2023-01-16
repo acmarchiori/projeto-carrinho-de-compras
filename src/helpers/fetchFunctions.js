5fbf3351390a141e37f9e149aec3c4c8f2756d29
acmarchiori <ac_marchiori@yahoo.com.br>
2023-01-16 18:34:00 -0300

    Corrigido requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -3,14 +3,14 @@ export const fetchProduct = () => {
 };
 
 export const fetchProductsList = async (QUERY) => {
+  if (!QUERY) {
+    throw new Error('Termo de busca não informado');
+  }
   try {
     const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
     const dados = await response.json();
     return dados.results;
   } catch (error) {
-    if (!QUERY) {
-      throw new Error('Termo de busca não informado');
-    }
     return error;
   }
 };
