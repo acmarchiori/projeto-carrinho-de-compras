6958480dc639be4899406aac4d780b5f9c51de68
acmarchiori <ac_marchiori@yahoo.com.br>
2023-01-16 21:01:11 -0300

    Finalizado requisito 4

--- src/main.js
+++ src/main.js
@@ -5,13 +5,23 @@ import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const products = document.querySelector('.products');
+const container = document.querySelector('.container');
+
+const createLoading = () => {
+  const paragraph = document.createElement('p');
+  paragraph.className = 'loading';
+  paragraph.innerText = 'carregando...';
+  products.appendChild(paragraph);
+};
 
 const getProducts = async () => {
+  createLoading();
   const product = await fetchProductsList('computador');
   product.forEach((e) => {
     const data = createProductElement(e);
-    console.log(data);
     products.appendChild(data);
   });
+  const delParagraph = document.querySelector('.loading');
+  delParagraph.remove();
 };
 getProducts();
