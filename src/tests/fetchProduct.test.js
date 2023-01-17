603d71a7e2dafd8d5aa3c0426f136bf5528e6785
acmarchiori <ac_marchiori@yahoo.com.br>
2023-01-16 21:42:36 -0300

    Finalizado requisito 6

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,20 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('fetchProduct é uma função', () => {
+    expect(typeof fetchProduct).toBe('function');
+  });
+
+  it('fetch é chamado com o endpoint correto ao executar fetchProduct', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561'); 
+  });
+
+  it('fetchProduct com o argumento "MLB1405519561" é uma estrutura de dados igual ao objeto product', async () => {
+    expect(await fetchProduct('MLB1405519561')).toEqual(product); 
+  });
+
+  it('ao chamar a função fetchProduct sem argumento, retorna um erro ', async () => {
+    await expect(fetchProduct()).rejects.toThrow('ID não informado');
   });
 });
