033b57641f2665d3d0dcb5f1131090b8e89174ac
acmarchiori <ac_marchiori@yahoo.com.br>
2023-01-19 14:56:19 -0300

    Finalizado requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,10 +1,24 @@
-export const getAddress = async () => {
-  // const API1 = fetch(`https://cep.awesomeapi.com.br/json/${CEP}`);
-  // const API2 = fetch(`https://brasilapi.com.br/api/cep/v2/${CEP}`);
-  // const cepAddress = await Promise.any([API1, API2]);
-  // const handledAddress = await cepAddress.json();
+const errorMessage = 'CEP não encontrado';
+
+export const getAddress = async (CEP) => {
+  const API1 = fetch(`https://cep.awesomeapi.com.br/json/${CEP}`);
+  const API2 = fetch(`https://brasilapi.com.br/api/cep/v2/${CEP}`);
+  const cepAddress = await Promise.any([API1, API2]);
+  const handledAddress = await cepAddress.json();
+  if (handledAddress.code === 'invalid' || handledAddress.code === 'not_found') {
+    throw new Error(errorMessage);
+  }
+  return handledAddress;
 };
 
 export const searchCep = async () => {
-
+  const cartAddress = document.querySelector('.cart__address');
+  try {
+    const imput = document.querySelector('.cep-input');
+    const address = await getAddress(imput.value);
+    cartAddress.innerHTML = `${address.street || address.address} - ${address.neighborhood
+      || address.district} - ${address.city} - ${address.state}`;
+  } catch (error) {
+    cartAddress.innerHTML = errorMessage;
+  }
 };
